58cff28860e435b6dae6933494cbffee0684ab92
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-13 11:42:27 +0100

    initial commit

---
+++ internal/app/dependencies/dependencies.go
@@ -0,0 +1,15 @@
+package dependencies
+
+// import (
+// 	// "jolo-mars/config"
+// )
+
+// type Dependencies struct {
+// 	Config *config.Config
+// }
+
+// func NewDependencies() *Dependencies {
+// 	return &Dependencies{
+// 		Config: config.LoadConfig(),
+// 	}
+// }
