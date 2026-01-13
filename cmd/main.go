fc08336637110e5a4393b0f4b2c0229788b3cae9
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-13 12:05:18 +0100

    initial commi

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"os"
 )
+
+
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run cmd/main.go <name>")
+		fmt.Println("Usage: go run cmd/main.go <Your Name>")
 		return
 	}
 	fmt.Printf("Hello, %s!\n", os.Args[1])
