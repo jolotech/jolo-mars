4871ef608b9052fbddc0feff220d0e7772f28953
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-15 17:50:24 +0100

    feat: databases and migraions

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
-module github.com/jolotech/jolo-mars.git
+module github.com/jolotech/jolo-mars
 
 go 1.25.3
+
+require (
+	gorm.io/driver/mysql v1.6.0
+	gorm.io/gorm v1.31.1
+)
+
+require github.com/google/uuid v1.6.0 // indirect
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/datatypes v1.2.7
+)
--- internal/domain/admin.go
+++ internal/domain/admin.go
@@ -1,1 +1,21 @@
 package domain
+
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/datatypes"
+)
+
+type Admin struct {
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `json:"name" gorm:"not null"`
+	Email     string         `json:"email" gorm:"unique;not null"`
+	Password  string         `json:"-" gorm:"not null"`
+	Role      string         `json:"role" gorm:"not null;default:'super-admin'"`
+	Actions   datatypes.JSON `json:"actions,omitempty" gorm:"type:json"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
