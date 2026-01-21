7d76e0e0b59db2733a1adb0a4136e45647f16fd6
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 05:19:22 +0100

    user auth

---
+++ internal/repository/admin/main.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+	"github.com/jolotech/jolo-mars/internal/models"
+)
+
+type AdminMainRepository struct {
+	db *gorm.DB
+}	
+
+
+func NewAdminMainRepository(db *gorm.DB) *AdminMainRepository {
+	return &AdminMainRepository{db: db}
+}
+
+
+func (r *AdminMainRepository) GetBusinessSetting(key string) interface{} {
+	var setting models.BusinessSetting
+	result := r.db.First(&setting)
+	if result.Error != nil {
+		return nil
+	}
+	switch key {
+	case "ref_earning_status":
+		return setting.RefEarningStatus
+	case "registration_bonus_status":
+		return setting.RegistrationBonusStatus
+	case "registration_bonus_amount":
+		return setting.RegistrationBonusAmount
+	case "service_charge_percent":
+		return setting.ServiceChargePercent
+	default:
+		return nil
+	}
+}
