e36f3f7c4821453947baa8cdb437bac73b48909b
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 04:03:29 +0100

    user auth

---
+++ internal/models/business_setings.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type BusinessSetting struct {
+	ID uint `gorm:"primaryKey"`
+
+	// Referral
+	RefEarningStatus        bool        `json:"ref_earning_status" gorm:"default:true"`
+
+	// Future settings (examples)
+	RegistrationBonusStatus bool        `json:"registration_bonus_status" gorm:"default:false"`
+	RegistrationBonusAmount float64     `json:"registration_bonus_amount" gorm:"default:0"`
+	ServiceChargePercent    float64     `json:"service_charge_percent" gorm:"default:0"`
+	CreatedAt               time.Time   `json:"created_at"`
+	UpdatedAt               time.Time   `json:"updated_at"`
+}
