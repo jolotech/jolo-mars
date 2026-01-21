2273139c57f1d6787eeda3bd0cc8fd85bdf40023
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 17:04:01 +0100

    feat: auth token

---
+++ internal/app/middlewares/user_auth.go
@@ -0,0 +1,67 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jolotech/jolo-mars/internal/helpers"
+)
+
+func UserAuthMiddleware() gin.HandlerFunc {
+	secret := os.Getenv("JWT_SECRET")
+
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			helpers.ErrorResponse(c, nil, "Missing Authorization header", http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			helpers.ErrorResponse(c, nil, "Authorization header must start with 'Bearer '", http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimSpace(strings.TrimPrefix(authHeader, "Bearer "))
+		log.Println("Extracted Token:", tokenString)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return secret, nil
+		})
+
+		if err != nil || !token.Valid {
+			helpers.ErrorResponse(c, err, "Invalid or expired token", http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			helpers.ErrorResponse(c, nil, "Invalid token claims", http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		email, ok := claims["email"].(string)
+		userId, ok := claims["user_id"]
+
+		if !ok || email == "" {
+			helpers.ErrorResponse(c, nil, "Invalid token payload", http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		// pass email forward
+		c.Set("userEmail", email)
+		c.Set("userId", userId)
+
+		c.Next()
+	}
+}
