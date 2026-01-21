e928aa49d139cef8cc688565bcd2f100350fd2c8
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 22:48:59 +0100

    Otp Verification

---
+++ types/auth.go
@@ -0,0 +1,21 @@
+package types
+
+
+type RegisterRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+	RefCode  string `json:"ref_code"`
+}
+
+type LoginSettings struct {
+	ManualLogin       bool `json:"manual_login_status"`
+	OtpLogin          bool `json:"otp_login_status"`
+	SocialLogin       bool `json:"social_login_status"`
+	GoogleLogin       bool `json:"google_login_status"`
+	FacebookLogin     bool `json:"facebook_login_status"`
+	AppleLogin        bool `json:"apple_login_status"`
+	EmailVerification bool `json:"email_verification_status"`
+	PhoneVerification bool `json:"phone_verification_status"`
+}
