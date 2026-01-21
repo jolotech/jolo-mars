82ed98c4256f5c07a0527f82810b9faf317af665
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 15:40:25 +0100

    hash pass

---
+++ internal/utils/password.go
@@ -0,0 +1,24 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash password (SIGNUP)
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost, // cost = 10
+	)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
+
+// Compare password (LOGIN)
+func ComparePassword(hashedPassword, rawPassword string) bool {
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword),
+		[]byte(rawPassword),
+	)
+	return err == nil
+}
