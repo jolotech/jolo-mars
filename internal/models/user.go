520f6ba46e49444089dcd36a0d6619c29bc557cc
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 04:36:42 +0100

    user auth

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,16 +4,16 @@ package models
 import "time"
 
 type User struct {
-	ID               uint      `gorm:"primaryKey"`
-	FName            string    `gorm:"column:f_name"`
-	LName            string    `gorm:"column:l_name"`
-	Email            string    `gorm:"unique"`
-	Phone            string    `gorm:"unique"`
-	Password         string
-	RefBy            *uint     `gorm:"column:ref_by"`
-	RefCode          string    `gorm:"column:ref_code"`
-	Status           bool
-	CMFirebaseToken  *string   `gorm:"column:cm_firebase_token"`
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               uint      `json:"id" gorm:"primaryKey"`
+	FName            string    `json:"f_name" gorm:"column:f_name"`
+	LName            string    `json:"l_name" gorm:"column:l_name"`
+	Email            string    `json:"email" gorm:"unique"`
+	Phone            string    `json:"phone" gorm:"unique"`
+	Password         string    `json:"-" gorm:"not null"`
+	RefBy            *uint     `json:"ref_by" gorm:"column:ref_by"`
+	RefCode          string    `json:"ref_code" gorm:"column:ref_code"`
+	Status           bool      `json:"status" gorm:"default:true"`
+	CMFirebaseToken  *string   `json:"cm_firebase_token" gorm:"column:cm_firebase_token"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
