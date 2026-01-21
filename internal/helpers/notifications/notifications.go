ec77b62f193ec1aff8522c32360e32a2ba9fe342
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-21 07:42:41 +0100

    notifications

---
+++ internal/helpers/notifications/notifications.go
@@ -0,0 +1,76 @@
+package helpers
+
+
+import (
+	"strings"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"gorm.io/gorm"
+	"github.com/jolotech/jolo-mars/internal/models"
+	"github.com/jolotech/jolo-mars/types"
+)
+
+
+
+
+func GetNotificationStatusData(db *gorm.DB, userType string, key string, notificationType string, storeID *uint,) bool {
+
+	var setting models.NotificationSetting
+
+	query := db.Where("type = ?", userType).Where("key = ?", key)
+
+	// Store-specific override
+	if storeID != nil && userType == "store" {
+		query = query.Where("store_id = ?", *storeID)
+	}
+
+	if err := query.First(&setting).Error; err != nil {
+		return false
+	}
+
+	var status string
+
+	switch notificationType {
+	case "push_notification_status":
+		status = setting.PushNotificationStatus
+	case "email_notification_status":
+		status = setting.EmailNotificationStatus
+	case "sms_notification_status":
+		status = setting.SmsNotificationStatus
+	default:
+		return false
+	}
+
+	return strings.ToLower(status) == "active"
+}
+
+
+
+func SendPushNotifToDevice(sa types.FirebaseServiceAccount, fcmToken string, data map[string]interface{}, webPushLink ...string,) error {
+
+	clickAction := ""
+	if len(webPushLink) > 0 {
+		clickAction = webPushLink[0]
+	}
+
+	accessToken, err := getAccessToken(sa)
+	if err != nil {
+		return err
+	}
+
+	payload := buildFCMPayload(fcmToken, data, clickAction)
+	body, _ := json.Marshal(payload)
+
+	req, _ := http.NewRequest(
+		"POST",
+		"https://fcm.googleapis.com/v1/projects/"+sa.ProjectID+"/messages:send",
+		bytes.NewBuffer(body),
+	)
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	_, err = http.DefaultClient.Do(req)
+	return err
+}
