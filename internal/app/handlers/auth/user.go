ce525a2fa45bc349e66b414ab43f8081bbd85ef7
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-22 06:29:28 +0100

    user auth

---
+++ internal/app/handlers/auth/user.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jolotech/jolo-mars/internal/helpers"
+	services "github.com/jolotech/jolo-mars/internal/services/user"
+	"github.com/jolotech/jolo-mars/types"
+)
+
+type UserAuthHandler struct {
+	UserAuthService *services.UserAuthService
+}
+
+func NewUserAuhHandler(userAuthService *services.UserAuthService) *UserAuthHandler {
+	return &UserAuthHandler{
+		UserAuthService: userAuthService,
+	}
+}
+
+func (h *UserAuthHandler) Register(c *gin.Context) {
+	var req types.RegisterRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err.Error(),
+		})
+		return
+	}
+
+	msg, data, statusCode, err := h.UserAuthService.Register(c, req)
+	if err != nil {
+		helpers.ErrorResponse(c, err, msg, statusCode)
+		return
+	}
+	helpers.SuccessResponse(c, data, msg, statusCode)
+}
