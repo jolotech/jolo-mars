d5defe094fe9f1052dcf771dba3ad372e1bec287
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-26 17:55:30 +0100

    send email

---
+++ internal/helpers/email/methods.go
@@ -0,0 +1,21 @@
+package email
+
+func (s *Sender) Verification() error {
+	body := verificationTemplate(s.User.Name, s.OTP)
+	return sendMail(s.User.Email, "Email Verification", body)
+}
+
+func (s *Sender) Welcome() error {
+	body := welcomeTemplate(s.User.Name)
+	return sendMail(s.User.Email, "Welcome 🎉", body)
+}
+
+func (s *Sender) ForgetPassword() error {
+	body := forgetPasswordTemplate(s.User.Name, s.Token)
+	return sendMail(s.User.Email, "Forgot Password", body)
+}
+
+func (s *Sender) ResetPassword() error {
+	body := resetPasswordTemplate(s.User.Name)
+	return sendMail(s.User.Email, "Password Reset Successful", body)
+}
