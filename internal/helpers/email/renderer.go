d9b5d86d6d7e6505f41f19b1453efcd451481473
Abdullahi <isihaqabdullahi01@gmail.com>
2026-01-26 18:18:32 +0100

    send email

--- internal/helpers/email/renderer.go
+++ internal/helpers/email/renderer.go
@@ -1,2 +1,24 @@
 package email
 
+
+import (
+	"bytes"
+	"html/template"
+	"path/filepath"
+)
+
+func renderTemplate(templateName string, data any) (string, error) {
+	path := filepath.Join("internal", "email", "templates", templateName)
+
+	tpl, err := template.ParseFiles(path)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	if err := tpl.Execute(&buf, data); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
