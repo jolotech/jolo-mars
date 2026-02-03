e1d53165d339053b533a4f1d3c953221c980a2a8
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-03 09:58:55 +0100

    feat: updated

--- internal/helpers/validations/auth_validation.go
+++ internal/helpers/validations/auth_validation.go
@@ -8,31 +8,6 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-// func HandleValidationError(c *gin.Context, err error) string {
-// 	errs, ok := err.(validator.ValidationErrors)
-// 	if !ok {
-// 		return "Validation failed"
-// 	}
-
-// 	errors := make(map[string]string)
-
-// 	for _, e := range errs {
-// 		field := strings.ToLower(e.Field())
-// 		switch e.Tag() {
-// 		case "required":
-// 			errors[field] = field + " is required"
-// 		case "min":
-// 			errors[field] = field + " must be at least " + e.Param() + " characters"
-// 		default:
-// 			errors[field] = "invalid " + field
-// 		}
-// 	}
-
-	
-
-// 	return true
-// }
-
 
 func HandleValidationError(err error) string {
 	errs, ok := err.(validator.ValidationErrors)
