71f76334cd33a6cc31d405d050f1b67fe2975b7a
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-03 08:52:53 +0100

    guest request

---
+++ internal/services/guest/auth.go
@@ -0,0 +1,30 @@
+package guest_service
+
+import (
+	"net/http"
+
+	"github.com/jolotech/jolo-mars/internal/models"
+	"github.com/jolotech/jolo-mars/internal/repository/guest"
+	"github.com/jolotech/jolo-mars/types"
+)
+
+
+type GuestService struct {
+	guestRepo guest_repo.GuestRepo
+}
+
+func NewGuestService(guestRepo guest_repo.GuestRepo) *GuestService {
+	return &GuestService{guestRepo: guestRepo}
+}
+
+// ================= CREATE GUEST ===============
+func (s *GuestService) GuestRequest(req types.GuestRequest) (string, any, int, error) {
+	guest := &models.Guest{
+		IPAddress: req.IPAddress,
+		FCMToken:  req.FCMToken,
+	}
+	if err := s.guestRepo.CreateGuest(guest); err != nil {
+		return "failed", nil, http.StatusInternalServerError, err
+	}
+	return "guest verified", types.GuestResponse{GuestID: guest.ID}, http.StatusOK, nil
+}
