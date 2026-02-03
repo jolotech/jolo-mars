8735d22a16cab59d94bacc1c592cb6b64af521f9
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-03 08:25:16 +0100

    guest request

--- internal/repository/user/auth.go
+++ internal/repository/user/auth.go
@@ -34,6 +34,16 @@ func (r *Auth) CreateUser(user *models.User) (*models.User, error) {
 	return user, nil
 }
 
+func (r *Auth) Create(guest *models.Guest) (*models.Guest, error) {
+	guest.CreatedAt = time.Now()
+	guest.UpdatedAt = time.Now()
+	if err := r.db.Create(guest).Error; err != nil {
+		return nil, err
+	}
+	return guest, nil
+}
+
+
 func (r *Auth) FindUserByRefCode(code string) (*models.User, error) {
 	var user models.User
 	err := r.db.Where("ref_code = ?", code).First(&user).Error
