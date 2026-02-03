7ff1155b1331760a7ab28da43b56fca0c5e9671f
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-03 08:46:38 +0100

    guest request

---
+++ internal/repository/guest/auth.go
@@ -0,0 +1,27 @@
+package repo
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/jolotech/jolo-mars/internal/models"
+)
+
+
+type GuestRepo struct {
+	db *gorm.DB
+}
+
+func NewGuestRepo(db *gorm.DB) *GuestRepo {
+	return &GuestRepo{db: db}
+}
+
+func (r *GuestRepo) CreateGuest(guest *models.Guest) error {
+	guest.CreatedAt = time.Now()
+	guest.UpdatedAt = time.Now()
+	if err := r.db.Create(guest).Error; err != nil {
+		return err
+	}
+	return  nil
+}
