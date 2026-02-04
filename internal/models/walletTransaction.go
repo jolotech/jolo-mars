0c469e96419546fc6f4ed09e26f312bbd31c9079
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-04 09:55:00 +0100

    feat: hide primaryKeyId

--- internal/models/walletTransaction.go
+++ internal/models/walletTransaction.go
@@ -1,10 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+	"github.com/google/uuid"
+)
 
 type WalletTransaction struct {
-	ID uint `gorm:"primaryKey"`
-
+	// ID uint `gorm:"primaryKey"`
+    ID               uint      `gorm:"primaryKey" json:"-"`
+	PublicID         uuid.UUID `gorm:"type:char(36);uniqueIndex;not null" json:"public_id"`
 	UserID    uint
 	Reference string `gorm:"index"` // phone stored here
 	Amount    float64
