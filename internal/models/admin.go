e1982363a419cb3cac872eaf19412ced5ddcf1ad
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-04 09:51:37 +0100

    feat: hide primaryKeyId

--- internal/models/admin.go
+++ internal/models/admin.go
@@ -11,7 +11,9 @@ import (
 )
 
 type Admin struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
+	// ID        uint           `gorm:"primaryKey" json:"id"`
+	ID        uint      `gorm:"primaryKey" json:"-"`
+	PublicID  uuid.UUID `gorm:"type:char(36);uniqueIndex;not null" json:"public_id"`
 	Name      string         `json:"name" gorm:"not null"`
 	Email     string         `json:"email" gorm:"unique;not null"`
 	Password  string         `json:"-" gorm:"not null"`
