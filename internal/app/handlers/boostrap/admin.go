f427c2701c8f24e320f36d32afeb97312cf7c8f6
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-06 17:43:45 +0100

    admin auth

---
+++ internal/app/handlers/boostrap/admin.go
@@ -0,0 +1,40 @@
+package bootstrap_handler
+
+import (
+	"log"
+
+	"github.com/jolotech/jolo-mars/internal/services/boostrap"
+
+
+)
+
+type BootstrapHandler struct {
+	service *bootstrap_service.BootstrapService
+}
+
+func NewBootstrapHandler(service *bootstrap_service.BootstrapService) *BootstrapHandler {
+	return &BootstrapHandler{service: service}
+}
+
+// Run on app start
+func (h *BootstrapHandler) Run() {
+	res, err := h.service.EnsureSuperAdminFromEnvSilently()
+	if err != nil {
+		// Your request says "fails silently" when admin exists.
+		// But if we get a real error (DB down), log it so you know.
+		log.Printf("[bootstrap] super admin bootstrap error: %v\n", err)
+		return
+	}
+
+	if res != nil && res.Created {
+		log.Println("[bootstrap] super admin created successfully")
+
+		// If we generated a password, print it ONCE to server logs.
+		// If you don't want this in logs, remove this.
+		if res.TempPassword != "" {
+			log.Printf("[bootstrap] TEMP PASSWORD (copy now): %s\n", res.TempPassword)
+		} else {
+			log.Println("[bootstrap] password came from SUPER_ADMIN_PASSWORD env")
+		}
+	}
+}
