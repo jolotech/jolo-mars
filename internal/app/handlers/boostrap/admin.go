6b288e28ad02b65ee4841358632b8c46b0dcd305
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-06 21:08:26 +0100

    boostrap admin

--- internal/app/handlers/boostrap/admin.go
+++ internal/app/handlers/boostrap/admin.go
@@ -17,59 +17,6 @@ func NewBootstrapHandler(service *bootstrap_service.BootstrapService) *Bootstrap
 }
 
 // Run on app start
-// func (h *BootstrapHandler) Run() {
-// 	log.Println("[bootstrap] starting super admin bootstrap...")
-
-// 	res, err := h.service.EnsureSuperAdminFromEnvSilently()
-// 	if err != nil {
-// 		// Your request says "fails silently" when admin exists.
-// 		// But if we get a real error (DB down), log it so you know.
-// 		log.Printf("[bootstrap] super admin bootstrap error: %v\n", err)
-// 		return
-// 	}
-
-// 	if res != nil && res.Created {
-// 		log.Println("[bootstrap] super admin created successfully")
-
-// 		// If we generated a password, print it ONCE to server logs.
-// 		// If you don't want this in logs, remove this.
-// 		if res.TempPassword != "" {
-// 			log.Printf("[bootstrap] TEMP PASSWORD (copy now): %s\n", res.TempPassword)
-// 		} else {
-// 			log.Println("[bootstrap] password came from SUPER_ADMIN_PASSWORD env")
-// 		}
-// 	}
-// }
-
-
-
-// func (h *BootstrapHandler) Run() {
-// 	log.Println("[bootstrap] starting super admin bootstrap...")
-
-// 	res, err := h.service.EnsureSuperAdminFromEnvSilently()
-// 	if err != nil {
-// 		log.Printf("[bootstrap] error: %v\n", err)
-// 		return
-// 	}
-
-// 	if res == nil {
-// 		log.Println("[bootstrap] skipped (nil result)")
-// 		return
-// 	}
-
-// 	if !res.Created {
-// 		log.Printf("[bootstrap] skipped: %s\n", res.Reason)
-// 		return
-// 	}
-
-// 	log.Println("[bootstrap] ✅ super admin created successfully")
-// 	if res.TempPassword != "" {
-// 		log.Printf("[bootstrap] TEMP PASSWORD (copy now): %s\n", res.TempPassword)
-// 	} else {
-// 		log.Println("[bootstrap] password came from SUPER_ADMIN_PASSWORD env")
-// 	}
-// }
-
 
 func (h *BootstrapHandler) Run() {
 	log.Println("[bootstrap] starting super admin bootstrap...")
