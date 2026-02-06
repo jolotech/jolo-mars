ae6389f972e1af07b789d356805ac0b0c2ac92ad
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-06 12:29:59 +0100

    mearge guest cart with user cart

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,24 +12,23 @@ type User struct {
 	ID               uint      `gorm:"primaryKey" json:"-"`
 	PublicID         string    `gorm:"type:char(15);uniqueIndex;not null" json:"public_id "`
 
-	FName            string    `json:"f_name" gorm:"column:f_name"`
-	LName            string    `json:"l_name" gorm:"column:l_name"`
-	Email            string    `json:"email" gorm:"unique"`
-	Phone            string    `json:"phone" gorm:"unique"`
-	Password         string   `json:"-" gorm:"column:password"`
-	PasswordHash     string   `json:"-" gorm:"column:password_hash"`
-	RefBy            *uint    `json:"ref_by" gorm:"column:ref_by"`
-	RefCode          string    `json:"ref_code" gorm:"type:varchar(100);uniqueIndex"`
-	Status           bool      `json:"status" gorm:"default:false"`
-	IsNew            bool      `json:"is_new" gorm:"default:true"`
-	IsPhoneVerified  bool      `json:"is_phone_verified" gorm:"column:is_phone_verified;default:false"`
-	IsEmailVerified  bool      `json:"is_email_verified" gorm:"column:is_email_verified;default:false"`
-	CMFirebaseToken  *string   `json:"cm_firebase_token" gorm:"column:cm_firebase_token"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	FName              string    `json:"f_name" gorm:"column:f_name"`
+	LName              string    `json:"l_name" gorm:"column:l_name"`
+	Email              string    `json:"email" gorm:"unique"`
+	Phone              string    `json:"phone" gorm:"unique"`
+	Password           string    `json:"-" gorm:"column:password"`
+	PasswordHash       string    `json:"-" gorm:"column:password_hash"`
+	RefBy              *uint     `json:"ref_by" gorm:"column:ref_by"`
+	RefCode            string    `json:"ref_code" gorm:"type:varchar(100);uniqueIndex"`
+	Status             bool      `json:"status" gorm:"default:false"`
+	// requireExistingOtp bool      `json:"require_existing_otp" gorm:"default:true"`
+	IsNew              bool      `json:"is_new" gorm:"default:true"`
+	IsPhoneVerified    bool      `json:"is_phone_verified" gorm:"column:is_phone_verified;default:false"`
+	IsEmailVerified    bool      `json:"is_email_verified" gorm:"column:is_email_verified;default:false"`
+	CMFirebaseToken    *string   `json:"cm_firebase_token" gorm:"column:cm_firebase_token"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
 }
-
-
 
 func (g *User) BeforeCreate(tx *gorm.DB) (err error) {
 	if g.PublicID == "" {
