85c649233501c9617584efe827e9b32af49cc7f4
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-08 02:14:04 +0100

    feat: super admin

---
+++ internal/repository/boostrap/admin.go
@@ -0,0 +1,28 @@
+package admin_repository
+
+import (
+
+	"github.com/jolotech/jolo-mars/internal/models"
+	"gorm.io/gorm"
+)
+
+type AdminBoostrap struct {
+	db *gorm.DB
+}
+
+func NewAdminBoostrap(db *gorm.DB) *AdminBoostrap {
+	return &AdminBoostrap{db: db}
+}
+
+func (r *AdminBoostrap) AnySuperAdminExists() (bool, error) {
+	var count int64
+	err := r.db.Model(&models.Admin{}).
+		Where("role = ?", "super-admin").
+		Where("deleted_at IS NULL").
+		Count(&count).Error
+	return count > 0, err
+}
+
+func (r *AdminBoostrap) CreateSuperAdmin(a *models.Admin) error {
+	return r.db.Create(a).Error
+}
