da0760f91f1f9afcae9abf4c6f87ef374eaa54ac
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-08 05:47:55 +0100

    admin login

--- internal/app/router/routes/admin.go
+++ internal/app/router/routes/admin.go
@@ -1,1 +1,30 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/jolotech/jolo-mars/internal/app/handlers/auth"
+)
+
+
+func AdminRoutes(
+	r *gin.Engine,
+	authHandler *auth.AdminAuthHandler,
+	// orderHandler *partners.OrderHandler,
+	// partnerRepo *repositories.PartnerRepository,
+	// distanceHandler *partners.DistanceHandler,
+	// auditService *service.AuditService,
+	// adminRepo *repositories.AdminRepository,
+	// paymentHandler *partners.PaymentHandler,
+	// StoreHandler *partners.StoreHandler,
+) {
+
+		public := r.Group("/admin")
+		{
+			auth := public.Group("/auth")
+			{
+				auth.POST("/login", authHandler.Login)
+			}
+		}
+
+}
