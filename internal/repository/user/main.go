ee8967e9c32f2bd56f9f864fffa153e59420383c
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-11 01:35:14 +0100

    login user

--- internal/repository/user/main.go
+++ internal/repository/user/main.go
@@ -98,7 +98,20 @@ func (r *Main) FindByPublicID(userPublicID string) (*models.User, error) {
 	return &user, nil
 }
 
+func (r *Main) EnsureRefCode(userID uint, refCode string) (bool, error) {
+	if refCode == "" {
+		return false, errors.New("refCode cannot be empty")
+	}
 
+	res := r.db.Model(&models.User{}).
+		Where("id = ? AND (ref_code IS NULL OR ref_code = '')", userID).
+		Update("ref_code", refCode)
+
+	if res.Error != nil {
+		return false, res.Error
+	}
+	return res.RowsAffected > 0, nil
+}
 
 // func MergeGuestCart(db *gorm.DB, userID uint, guestID string) bool {
 // 	if guestID == "" || userID == 0 {
