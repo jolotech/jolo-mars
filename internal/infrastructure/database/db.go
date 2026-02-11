e8734783283a850d01b63605362905e931ccedfe
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-11 19:25:33 +0100

    github actions

--- internal/infrastructure/database/db.go
+++ internal/infrastructure/database/db.go
@@ -16,11 +16,11 @@ var DB *gorm.DB
 func ConnectDB() {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
+		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("DB_DATABASE"),
 	)
 
 	// Configure GORM logger to show only errors
