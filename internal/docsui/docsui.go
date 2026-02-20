3039be9be5895e086e05ea02d033bea79fc5a696
Abdullahi <isihaqabdullahi01@gmail.com>
2026-02-20 12:34:49 +0100

    clean up docsui

--- internal/docsui/docsui.go
+++ internal/docsui/docsui.go
@@ -3,4 +3,6 @@ package docsui
 import "embed"
 
 //go:embed public/*
+// EmbeddedAssets is the embedded filesystem containing all static assets (CSS, JS, images) 
+// for the documentation UI.
 var EmbeddedAssets embed.FS
